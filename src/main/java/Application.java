6cb731ae23ec9edb53e56c267326f3350293a636
helloworld <ura22051995@gmail.com>
2023-05-16 23:07:10 +0300

    Выполнена задача №1

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -1,2 +1,29 @@
+import java.sql.*;
+
 public class Application {
+
+    public static void main(String[] args) throws SQLException {
+
+        final String user = "postgres";
+        final String password = "prikol";
+        final String url = "jdbc:postgresql://localhost:5432/skypro";
+
+        try (Connection connection = DriverManager.getConnection(url, user, password);
+             PreparedStatement statement = connection.prepareStatement("SELECT employee.first_name, employee.last_name, " +
+                     "employee.gender, city.city_name " +
+                     "FROM employee LEFT JOIN city ON city.city_id = employee.city_id WHERE id = (?)")
+        ) {
+            statement.setInt(1,9);
+            final ResultSet resultSet = statement.executeQuery();
+            while (resultSet.next()) {
+                System.out.printf("Name - %s, surname - %s, gender - %s, city - %s",
+                        resultSet.getString("first_name"),
+                        resultSet.getString("last_name"),
+                        resultSet.getString("gender"),
+                        resultSet.getString("city_name"));
+            }
+
+        }
+
+    }
 }
